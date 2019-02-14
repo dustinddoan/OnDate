f3c39467fc269e05c423809257d61267307a8eb4
Dustin Doan <dustin.ddoan@gmail.com>
2019-02-14 11:13:33 -0800

    initial setup

---
+++ OnDateAPI/Data/DataContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using OnDateAPI.Models;
+
+namespace OnDateAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        // DbContextOption with type DataContext change to base constructor
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+            
+        }
+
+        // tell EF about the entity, type DbSet type Value, Values=> table name
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ OnDateAPI/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace OnDateAPI.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
